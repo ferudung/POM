40ec43864ccb70bad8f021a2f98bb683685769c8
ferudun gogus <ferudungogus2021@gmail.com>
2021-10-19 23:08:46 +0300

    v13

---
+++ src/test/java/test/day14/C01_ConcortOtelPositiveTest.java
@@ -0,0 +1,39 @@
+package test.day14;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ConcortHotelPage;
+import utilities.TestBase;
+
+public class C01_ConcortOtelPositiveTest extends TestBase {
+
+//1 ) Bir Class olustur : PositiveTest
+
+
+
+
+
+
+
+    //2) Bir test method olustur positiveLoginTest()
+@Test
+    public void positiveLoginTest(){
+    // https://www.concorthotel.com/ adresine git
+    driver.get("https://www.concorthotel.com/");
+    // login butonuna bas
+    ConcortHotelPage concortHotelPage=new ConcortHotelPage(driver);
+    concortHotelPage.ilkLoginLinki.click();
+    // test data username: manager ,
+    concortHotelPage.usernameKutusu.sendKeys("manager");
+    // test data password : Manager1!
+    concortHotelPage.passwordKutusu.sendKeys("Manager1!");
+    // Degerleri girildiginde sayfaya basarili sekilde
+    // girilebildigini test et
+    concortHotelPage.loginButonu.click();
+    Assert.assertTrue(concortHotelPage.basariliGirisYaziElementi.isDisplayed());
+
+
+
+}
+
+}
---
+++ src/test/java/utilities/ConfigReader.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    //1- Properties objesi olusturucağız
+
+    static  Properties properties;
+    //2- Bu classın amacı confiruguration.properties
+    // dosyasını okumak ve key value ikililerini kullanarak
+    // istediğimiz keye adi value'yu bize gertirmek
+    static {
+        String dosyaYolu="configuration.properties";
+        try {
+            FileInputStream fileInputStream=new FileInputStream(dosyaYolu);
+            properties=new Properties();
+            properties.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //3- Test classlarından configReader classına ulasıp
+    // yukardaki işlemleri yapmamızı saglayacak bir method olusturacağız
+
+public static String getProperty(String key){
+
+        String value=properties.getProperty(key);
+        return value;
+}
+}
